f0ffc53d335cf618aef5237f16384072df18ffc1
KubaP <zydmunt@gmail.com>
2022-10-06 14:24:59 +0200

    Update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,10 +12,16 @@ import java.time.Month;
 
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 
